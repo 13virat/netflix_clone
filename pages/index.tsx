790270ccf7fe6730c58bb3221390a2dd86dbd5a6
13virat <viratgupta1314@gmail.com>
2023-07-18 22:30:52 +0530

    bb

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import { useEffect } from "react";
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -30,6 +31,7 @@ export default function Home() {
   return (
     <>
      <Navbar />
+     <Billboard />
     </>
   );
 }
